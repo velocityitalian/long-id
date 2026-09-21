20d51f8b0378157b62de226d67077e73551e02b8
velocityitalian <velocityitalian@gmail.com>
2026-09-22 01:08:48 +0530

    Keep clear narrator voice: en-US-GuyNeural

--- youtube_longform_automation.py
+++ youtube_longform_automation.py
@@ -78,7 +78,7 @@ CATEGORIES_INDONESIAN = {
 }
 
 # Edge TTS voices
-ENGLISH_VOICE = "en-US-AndrewNeural"
+ENGLISH_VOICE = "en-US-GuyNeural"
 LANG_VOICE = "id-ID-GadisNeural"
 
 # Phrase history file
